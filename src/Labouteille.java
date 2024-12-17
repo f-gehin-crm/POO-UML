d44e54ca65776fec2bd7fcd0668d879f4f594a24
f-gehin-crm <FGehin@arfp.asso.fr>
2024-12-17 12:54:55 +0100

    commit v1.0

---
+++ src/Labouteille.java
@@ -0,0 +1,26 @@
+public class Labouteille {
+
+    // attributs
+
+    private boolean ouvert;
+    private float contenantEnMl;
+    private String marque;
+    private float tailleEnCm;
+    private String materiaux;
+    private String contenu;
+    private float contenuEnPourcentage;
+    private float poidsEnGr;
+
+
+    // Constructeur par défaut
+
+    public Labouteille () {   
+    }
+
+    // Constructeur classique 
+    // This. sert à indiquer un objet particulier
+    public Labouteille (boolean ouvert) {
+        
+    }
+
+}
