d05f10daa56e1e0ce2a30d770fc840eaf3a3d102
f-gehin-crm <FGehin@arfp.asso.fr>
2024-12-17 15:48:10 +0100

    fin de journée

--- src/Labouteille.java
+++ src/Labouteille.java
@@ -1,4 +1,4 @@
-public class Labouteille {
+public class Labouteille implements Cloneable{
 
     // attributs
 
@@ -19,8 +19,43 @@ public class Labouteille {
 
     // Constructeur classique 
     // This. sert à indiquer un objet particulier
-    public Labouteille (boolean ouvert) {
+    public Labouteille (boolean ouvert, float contenantEnMl, 
+    String marque, float tailleEnCm, String materiaux, String contenu, float contenuEnPourcentage, float poidsEnGr) {
+        this.ouvert = ouvert;
+        this.contenantEnMl = contenantEnMl;
+        this.marque = marque;
+        this.tailleEnCm= tailleEnCm;
+        this.materiaux = materiaux;
+        this.contenu =contenu;
+        this.contenuEnPourcentage= contenuEnPourcentage;
+        this.poidsEnGr = poidsEnGr;
+    }
+
+        // Constructeur par clonage version jessy
+        public Object clone() {
+                Object o = null;
+                try {
+                    // On récupère l'instance à renvoyer par l'appel de la
+                    // méthode super.clone()
+                    o = super.clone();
+                } catch (CloneNotSupportedException cnse) {
+                    // Ne devrait jamais arriver, car nous implémentons
+                    // l'interface Cloneable
+                    cnse.printStackTrace(System.err);
+                }
+                // on renvoie le clone
+                return o;
+            
+        }
+
+    /*    // Constructeur par clonage 
+         public Labouteille ( Labouteille labouteille1) */
+
+
+
+
+
         
     }
 
-}
+
